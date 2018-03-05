6413d669f98f640b0d48a642f7dbef2a48bc7632
jiyinyiyong <jiyinyiyong@gmail.com>
2018-03-05 23:47:50 +0800

    first working example

--- src/app/comp/color_pad.cljs
+++ src/app/comp/color_pad.cljs
@@ -4,21 +4,85 @@
             [respo-ui.core :as ui]
             [respo.macros
              :refer
-             [defcomp cursor-> action-> mutation-> <> div button textarea span]]
+             [defcomp cursor-> action-> mutation-> list-> <> div button textarea span]]
             [verbosely.core :refer [verbosely!]]
             [respo.comp.space :refer [=<]]
             [reel.comp.reel :refer [comp-reel]]
             [respo-md.comp.md :refer [comp-md]]))
+
+(defn hsl100 [h100 s l] (hsl (* 2.4 h100) s l))
+
+(defcomp
+ comp-hundred
+ (color letter)
+ (let [weight (get color letter), digit (mod weight 10), decade (/ (- weight digit) 10)]
+   (div
+    {:style ui/row}
+    (div
+     {:style ui/row}
+     (list->
+      {}
+      (->> (range 10)
+           (map
+            (fn [i]
+              [i
+               (let [current-weight (+ digit (* 10 i))
+                     computed-color (case letter
+                                      :h (hsl100 current-weight (:s color) (:l color))
+                                      :s (hsl100 (:h color) current-weight (:l color))
+                                      :l (hsl100 (:h color) (:s color) current-weight)
+                                      :transparent)]
+                 (div
+                  {:style (merge
+                           ui/center
+                           {:width 40,
+                            :height 40,
+                            :background-color computed-color,
+                            :cursor :pointer}
+                           (if (= decade i) {:outline (str "1px solid red")})),
+                   :on-click (fn [e d! m!] (d! letter current-weight))}))]))))
+     (list->
+      {}
+      (->> (range 10)
+           (map
+            (fn [i]
+              [i
+               (let [current-weight (+ i (* decade 10))
+                     computed-color (case letter
+                                      :h (hsl100 current-weight (:s color) (:l color))
+                                      :s (hsl100 (:h color) current-weight (:l color))
+                                      :l (hsl100 (:h color) (:s color) current-weight)
+                                      :transparent)]
+                 (div
+                  {:style (merge
+                           ui/center
+                           {:width 40,
+                            :height 40,
+                            :background-color computed-color,
+                            :cursor :pointer}
+                           (if (= digit i) {:outline (str "1px solid red")})),
+                   :on-click (fn [e d! m!] (d! letter current-weight))}))]))))))))
 
 (defcomp
  comp-color-pad
  (color)
  (div
   {:style {:padding 16}}
-  (div {} (<> color))
   (div
    {:style ui/row}
    (div
-    {:style {:width 200,
-             :height 200,
-             :background-color (hsl (:h color) (:s color) (:l color))}}))))
+    {:style (merge
+             ui/center
+             {:width 400,
+              :height 400,
+              :background-color (hsl100 (:h color) (:s color) (:l color)),
+              :color (if (> (:l color) 50) :black :white),
+              :font-family ui/font-code,
+              :font-size 24})}
+    (<> (str "hsl(" (:h color) ", " (:s color) "%, " (:l color) "%)")))
+   (=< 16 nil)
+   (comp-hundred color :h)
+   (=< 16 nil)
+   (comp-hundred color :s)
+   (=< 16 nil)
+   (comp-hundred color :l))))
--- src/app/comp/container.cljs
+++ src/app/comp/container.cljs
@@ -4,7 +4,7 @@
             [respo-ui.core :as ui]
             [respo.macros
              :refer
-             [defcomp cursor-> action-> mutation-> <> div button textarea span]]
+             [defcomp cursor-> action-> mutation-> <> div button textarea span a]]
             [verbosely.core :refer [verbosely!]]
             [respo.comp.space :refer [=<]]
             [reel.comp.reel :refer [comp-reel]]
@@ -14,9 +14,18 @@
 (defcomp
  comp-container
  (reel)
- (let [store (:store reel), states (:states store)]
+ (let [store (:store reel), states (:states store), color (:color store)]
    (div
-    {:style (merge ui/global)}
-    (<> (str "color: " (:color store)))
-    (comp-color-pad (:color store))
+    {:style (merge ui/center ui/fullscreen)}
+    (comp-color-pad color)
+    (a
+     {:style {:position :absolute,
+              :right 0,
+              :top 0,
+              :margin 8,
+              :font-family ui/font-fancy,
+              :font-size 16},
+      :href "https://github.com/Memkits/color-pad",
+      :target "_blank"}
+     (<> "Color Pad"))
     (cursor-> :reel comp-reel states reel {}))))
